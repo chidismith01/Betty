052c9c2bfe84e64ef9fc97a1e001fee6b4d6c04b
chidismith01 <emereuwachidiebere94@gmail.com>
2023-08-18 08:18:17 -0700

    a program of diferent types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *A program that prints the size of various types in c
+
+ * Return: 0 Always 
+ */
+int main(void)
+{
+	/* declaring variables of different types */
+	char n;
+	int m;
+	long int o;
+	long long int p;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(n));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(o));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
