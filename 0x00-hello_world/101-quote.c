73f0ee1a8715354437c22bac9c1797a8ea50d1d1
chidismith01 <emereuwachidiebere94@gmail.com>
2023-08-18 08:49:46 -0700

    write quote function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10\-19\n";
-	write(2, quote, sizeof(quote) -1);
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10
+\-19\n";
+	write(2, quote, sizeof(quote) - 1);
 	return (1);
-	return (1)
 }
