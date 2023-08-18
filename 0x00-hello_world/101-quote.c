05a3cad2f42aef02bea390b229bcc41aa878a60e
chidismith01 <emereuwachidiebere94@gmail.com>
2023-08-18 08:52:18 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,20 @@
 #include <unistd.h>
+
 /**
- * main - Entry Point
+ * main - Entry point
  *
  * A C program that prints exactly:
- * and that piece of art is useful" - Dora Korpar, 2015-10-19
- * followed by a new line, to the standard error.
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error
  *
- * Return : always 1 (error)
+ * Return: 1 always
  */
+
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10\-19\n";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10\
+-19\n";
+
 	write(2, quote, sizeof(quote) - 1);
 	return (1);
 }
