e06769cad3abfd774575c934480f6c5f53faadfc
chidismith01 <emereuwachidiebere94@gmail.com>
2023-08-18 08:44:14 -0700

    write quote function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main - Entry Point
+ *
+ * A C program that prints exactly:
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error.
+ *
+ * Return : always 1 (error)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10\-19\n";
+	write(2, quote, sizeof(quote) -1);
+	return (1);
+	return (1)
+}
